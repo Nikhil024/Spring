4cc40d5757d100e853f733a0ba43f15903114cd2
Nikhil <nikhil_mohandas@thbs.com>
2017-03-02 16:36:15 +0530

    Done with dashboard,profile

--- OnlineExamination/src/main/java/com/thbs/controllers/SelectExamController.java
+++ OnlineExamination/src/main/java/com/thbs/controllers/SelectExamController.java
@@ -1,15 +1,110 @@
 package com.thbs.controllers;
 
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.thbs.Beans.ImagesBean;
+import com.thbs.Beans.UserDetailsBean;
+import com.thbs.Beans.UsersBean;
+import com.thbs.Dao.ImagesDao;
+import com.thbs.Dao.UserDetailsDao;
+import com.thbs.Dao.UsersDao;
+import com.thbs.data.ExamConstants;
+import com.thbs.data.GetBeanContext;
+
 @Controller
 public class SelectExamController {
+	
+	@Autowired
+	UsersBean user;
+
+	@Autowired
+	ImagesBean images;
+	
+	@Autowired
+	UserDetailsBean userdetails;
+	
+	
+	GetBeanContext gbc = new GetBeanContext();
+	UsersDao userdao = gbc.getUserBeanContext();
+	ImagesDao imagesdao = gbc.getImagesBeanContext();
+	UserDetailsDao userdetailsdao = gbc.getuserDetailsBeanContext();
 	private final String VIEW_NAME = "selectexam"; 
+	private final String PROJECT_NAME = "projectname";
+	private final String BACKGROUND_IMAGE_NAME = "bgimagename";
+	private final String SESSION_AND_MODEL_EMAIL_VARIABLE = "email";
+	private final String USER_NAME = "username";
+	private final String MESSAGE_MODEL_ATTRIBUTE = "data";
+	private final String FAIL_MODEL_ATTRIBUTE = "fail";
+	private final String NO_LOGIN_VIEW_NAME = "login";
+	private final String CURRENT_PAGE_SESSION_NAME = "currentpage";
+	private final String LOCK_SCREEN_VIEW_NAME = "lockscreen";
+	private final String NO_PICTURE = "nopicture";
+	private final String PICTURE = "picture";
+	private final String USER_PROFILE_PICTURE = "userprofileimage";
 
 	@RequestMapping(value = "/selectexam",method = RequestMethod.GET)
-	public String getExam(){
+	public String getExam(Model model,HttpSession session){
+		model.addAttribute(PROJECT_NAME,ExamConstants.PROJECT_NAME);
+		model.addAttribute(BACKGROUND_IMAGE_NAME,ExamConstants.BACKGROUND_IMAGE);
+		
+		
+		
+		if(session.getAttribute(SESSION_AND_MODEL_EMAIL_VARIABLE)==null){
+			model.addAttribute(MESSAGE_MODEL_ATTRIBUTE,ExamConstants.NO_LOGIN_MESSAGE+" To Access The Profile Page.!");
+			model.addAttribute(FAIL_MODEL_ATTRIBUTE,ExamConstants.PAGE_DISPLAY_VALUE);
+			return NO_LOGIN_VIEW_NAME;
+		}
+		
+		
+		if(session.getAttribute(SESSION_AND_MODEL_EMAIL_VARIABLE)!=null){
+			List<UsersBean> users = userdao.getUserDetails(session.getAttribute(SESSION_AND_MODEL_EMAIL_VARIABLE).toString());
+
+
+			for(UsersBean u : users){
+				user.setId(u.getId());
+				user.setEmail(u.getEmail());
+				user.setName(u.getName());
+				user.setLastLogin(u.getLastLogin());
+				
+				List<ImagesBean> img = imagesdao.getAllImage(u.getId());
+				for(ImagesBean i :img){
+					images.setId(i.getId());
+					images.setName(i.getName());
+					images.setLocation(i.getLocation());
+					images.setUploadDate(i.getUploadDate());
+					images.setLastupdateDate(i.getLastupdateDate());
+				}
+			}
+			
+			if(session.getAttribute(CURRENT_PAGE_SESSION_NAME)!=null){
+			if(session.getAttribute(CURRENT_PAGE_SESSION_NAME).equals(LOCK_SCREEN_VIEW_NAME)){
+				model.addAttribute(MESSAGE_MODEL_ATTRIBUTE,ExamConstants.SCREEN_LOCKED_MESSAGE);
+				model.addAttribute(FAIL_MODEL_ATTRIBUTE,ExamConstants.PAGE_DISPLAY_VALUE);
+				if(images.getName() == null){
+					model.addAttribute(NO_PICTURE,ExamConstants.AFTER_VERIFICATION_VALUE);
+					model.addAttribute(USER_PROFILE_PICTURE,ExamConstants.FIRST_TIME_PROFILE_AND_NO_PROFILE_PICTURE); 
+					return LOCK_SCREEN_VIEW_NAME;
+				}
+				else{
+					model.addAttribute(PICTURE,ExamConstants.AFTER_VERIFICATION_VALUE);
+					model.addAttribute(USER_PROFILE_PICTURE,images.getName()+ExamConstants.JPEG_IMAGE_EXTENSION);
+					return LOCK_SCREEN_VIEW_NAME;
+				}
+			}
+			}
+			session.setAttribute(CURRENT_PAGE_SESSION_NAME, VIEW_NAME);
+		
+		model.addAttribute(USER_NAME,user.getName()); 
+		}
+		
 		return VIEW_NAME;
 	}
 	
--- OnlineExamination/src/main/webapp/WEB-INF/views/selectexam.jsp
+++ OnlineExamination/src/main/webapp/WEB-INF/views/selectexam.jsp
@@ -2,16 +2,67 @@
 	pageEncoding="ISO-8859-1"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
+<!doctype html>
+<html lang="en">
 <head>
-<jsp:include page="Header.jsp" />
-<title>Take Exam</title>
+	<jsp:include page="Header.jsp"/>
 </head>
 <body>
-	<h4>demo</h4>
-	<label class="checkbox"> 
-	<input type="checkbox" value="" data-toggle="checkbox">
-	</label>
+
+<div class="wrapper">
+    <div class="sidebar" data-color="orange" data-image="<spring:url value="/resources/img/${bgimagename}"/>">
+        <!--
+
+            Tip 1: you can change the color of the sidebar using: data-color="blue | azure | green | orange | red | purple"
+            Tip 2: you can also add an image using data-image tag
+
+        -->
+
+    	<div class="sidebar-wrapper">
+            <jsp:include page="sidebar.jsp"></jsp:include> 
+    	</div>
+    </div>
+    <div class="main-panel">
+	<div class="col-md-6">
+		<legend>Select Subject</legend>
+		<div class="row">
+			<div class="col-md-6">
+				<select name="cities" class="selectpicker"
+					data-title="Single Select" data-style="btn-default btn-block"
+					data-menu-style="dropdown-blue">
+					<option value="id">Bahasa Indonesia</option>
+					<option value="ms">Bahasa Melayu</option>
+					<option value="ca">Català</option>
+					<option value="da">Dansk</option>
+					<option value="de">Deutsch</option>
+					<option value="en">English</option>
+					<option value="es">Español</option>
+					<option value="el">Eλληνικά</option>
+					<option value="fr">Français</option>
+					<option value="it">Italiano</option>
+					<option value="hu">Magyar</option>
+					<option value="nl">Nederlands</option>
+					<option value="no">Norsk</option>
+					<option value="pl">Polski</option>
+					<option value="pt">Português</option>
+					<option value="fi">Suomi</option>
+					<option value="sv">Svenska</option>
+					<option value="tr">Türkçe</option>
+					<option value="is">Íslenska</option>
+					<option value="cs">Čeština</option>
+					<option value="ru">Русский</option>
+					<option value="th">ภาษาไทย</option>
+					<option value="zh">中文 (简体)</option>
+					<option value="zh-TW">中文 (繁體)</option>
+					<option value="ja">日本語</option>
+					<option value="ko">한국어</option>
+				</select>
+			</div>
+		</div>
+	</div>
+</div>
+</div>
+
+
 </body>
 </html>
